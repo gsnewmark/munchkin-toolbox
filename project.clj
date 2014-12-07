0cc368966a1bbf48dd3b0e1f0079545b5c8ccf5a
Ivan Kryvoruchko <gildraug@gmail.com>
2014-12-07 13:19:38 +0200

    Update project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject munchkin-toolbox "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject munchkin-toolbox "0.2.0-SNAPSHOT"
+  :description "A set of tools for Munchkin game"
+  :url "http://gsnewmark.github.io/munchkin-toolbox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.3.4"
